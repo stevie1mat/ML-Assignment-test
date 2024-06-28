0db96e09578a5117cb8490f26d979d5f55c66901
Steven Mathew <mathewsteven92@gmail.com>
2024-06-28 02:21:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,60 @@
-from flask import Flask
+from flask import Flask, request, render_template_string, redirect, url_for
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+from PIL import Image
+import io
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Steven Mathew'
+# Load the trained model
+model = load_model('fashion_mnist_model.h5')
 
+@app.route('/')
+def index():
+    with open('index.html', 'r') as file:
+        index_html = file.read()
+    return render_template_string(index_html)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return redirect(url_for('index'))
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return redirect(url_for('index'))
+
+    try:
+        # Read the image file
+        image = Image.open(file)
+
+        # Convert the image to grayscale
+        image = image.convert('L')
+
+        # Resize the image to 28x28 pixels
+        image = image.resize((28, 28))
+
+        # Convert the image to a numpy array
+        image_array = np.array(image)
+
+        # Normalize the image
+        image_array = image_array / 255.0
+
+        # Reshape the image for the model
+        image_array = image_array.reshape(1, 28, 28)
+
+        # Make a prediction
+        prediction = model.predict(image_array)
+        predicted_class = np.argmax(prediction, axis=1)[0]
+
+        # Render the result in the template
+        with open('index.html', 'r') as file:
+            index_html = file.read()
+        return render_template_string(index_html, prediction=predicted_class)
+
+    except Exception as e:
+        return str(e)
 if __name__ == '__main__':
     app.run()
